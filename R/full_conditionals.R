bf127b84b265bc99f0556326a50ee16b307436e9
wlandau <will.landau@gmail.com>
2015-12-06 14:57:34 -0600

    thin

--- R/full_conditionals.R
+++ R/full_conditionals.R
@@ -20,7 +20,7 @@ sample_full_conditionals = function(dir, scenario, starts = Starts(), priors = "
   nse = sample.int(N, 3)
   gse = sample.int(G, 4)
 
-  configs = Configs(burnin = 1e3, iterations = 1e3, thin = 10, priors = priors,
+  configs = Configs(burnin = 1e3, iterations = 1e4, thin = 0, priors = priors,
                                libraries_return = ns, genes_return = gs, libraries_return_epsilon = nse, 
                                genes_return_epsilon  = gse,
                                parameter_sets_return = "beta", parameter_sets_update = "beta")
