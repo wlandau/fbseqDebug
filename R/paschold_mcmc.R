09f9c446c0b17313ae7bd4a953bc380cfd367869
wlandau <will.landau@gmail.com>
2016-05-27 15:21:55 -0400

    Add new paschold_mcmc to OpenMP tests

--- R/paschold_mcmc.R
+++ R/paschold_mcmc.R
@@ -4,26 +4,18 @@
 #' @param prior alternate prior to try for the betas
 paschold_mcmc = function(prior = "normal"){
   data(paschold)
+  paschold@counts = paschold@counts[1:30,]
 
   dir = paste0("paschold_", prior)
   if(!file.exists(dir)) dir.create(dir)
   setwd(dir)
 
-  configs = Configs(priors = prior)
+  configs = Configs(priors = prior, iterations = 1000, burnin = 100, thin = 1)
   chain = Chain(paschold, configs)
   saveRDS(chain, paste0("chain_begin_", prior, ".rds"))
 
   chain = fbseq(chain, backend = "OpenMP", threads = 4)
   saveRDS(chain, paste0("chain_", prior, ".rds"))
-  flat = mcmc(mcmc_samples(chain))
-  pdf(paste0("trace_", prior, ".pdf"))
-  plot(flat, density = F)
-  dev.off()
-  pdf(paste0("density_", prior, ".pdf"))
-  plot(flat, trace = F)
-  dev.off()
-  pl = volcano(chain)
-  ggsave(filename = paste0("volcano_", prior, ".pdf"), plot = pl)
 
   setwd("..")
 }
